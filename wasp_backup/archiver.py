a9e11e4e427649fd109f41594d3bd8725b8dc487
Ildar Gafurov <dev@binblob.com>
2017-07-13 12:18:32 +0300

    wasp_backup/archiver.py: WLVMBackupTarArchiver class added

--- wasp_backup/archiver.py
+++ wasp_backup/archiver.py
@@ -31,9 +31,13 @@ import hashlib
 import os
 import tarfile
 import json
+import uuid
+import tempfile
 from enum import Enum
 
 from wasp_general.verify import verify_type, verify_value
+from wasp_general.os.linux.lvm import WLogicalVolume
+from wasp_general.os.linux.mounts import WMountPoint
 
 from wasp_launcher.apps import WAppsGlobals
 
@@ -46,8 +50,8 @@ class WBackupTarArchiver:
 		gzip = 'gz'
 		bzip2 = 'bz2'
 
-	@verify_type(archive_path=str)
-	@verify_value(archive_path=lambda x: len(x) > 0)
+	@verify_type(archive_path=str, backup_sources=str)
+	@verify_value(archive_path=lambda x: len(x) > 0, backup_sources=lambda x: len(x) > 0)
 	def __init__(self, archive_path, *backup_sources, compress_mode=None):
 		self.__archive_path = archive_path
 		self.__backup_sources = list(backup_sources)
@@ -113,3 +117,122 @@ class WBackupTarArchiver:
 		meta_data_keys.sort()
 		for key in meta_data_keys:
 			WAppsGlobals.log.debug('Archive meta information. %s: %s', str(key), str(meta_data[key]))
+
+
+class WLVMBackupTarArchiver(WBackupTarArchiver):
+
+	__default_snapshot_size__ = 0.1
+	__mount_directory_prefix__ = 'wasp-backup-'
+
+	@verify_type('paranoid', archive_path=str, backup_sources=str, compress_mode=WBackupTarArchiver.CompressMode)
+	@verify_value('paranoid', archive_path=lambda x: len(x) > 0, backup_sources=lambda x: len(x) > 0)
+	@verify_type(sudo=bool)
+	def __init__(self, archive_path, *backup_sources, compress_mode=None, sudo=False):
+		WBackupTarArchiver.__init__(self, archive_path, *backup_sources, compress_mode=compress_mode)
+		self.__sudo = sudo
+		self.__logical_volume_uuid = None
+		self.__snapshot = False
+
+	def sudo(self):
+		return self.__sudo
+
+	@verify_type(snapshot_force=bool, snapshot_size=(int, float, None), mount_directory=(str, None))
+	@verify_type(mount_fs=(str, None), mount_options=(list, tuple, set, None))
+	def archive(
+		self, snapshot_force=False, snapshot_size=None, mount_directory=None, mount_fs=None, mount_options=None
+	):
+		self.__logical_volume_uuid = None
+		self.__snapshot = False
+
+		logical_volume = None
+		backup_sources = self.backup_sources()
+
+		if len(backup_sources) == 0:
+			if snapshot_force is True:
+				raise RuntimeError('Unable to create snapshot for empty archive')
+			else:
+				WBackupTarArchiver.archive(self)
+				return
+
+		for source in backup_sources:
+			lv = WLogicalVolume.logical_volume(source, sudo=self.sudo())
+			if lv is None:
+				if snapshot_force is True:
+					raise RuntimeError('Unable to create snapshot for non-LVM volume')
+				logical_volume = None
+				break
+			if logical_volume is None:
+				logical_volume = lv
+			elif os.path.realpath(logical_volume.volume_path()) == os.path.realpath(lv.volume_path()):
+				pass
+			else:
+				if snapshot_force is True:
+					raise RuntimeError(
+						'Unable to create snapshot - files reside on different volumes'
+					)
+				logical_volume = None
+				break
+
+		if logical_volume is None:
+			if snapshot_force is True:
+				raise RuntimeError('Unable to create snapshot for unknown reason')
+			WBackupTarArchiver.archive(self)
+			return
+
+		if snapshot_size is None:
+			snapshot_size = self.__class__.__default_snapshot_size__
+
+		snapshot_suffix = '-snapshot-%s' % str(uuid.uuid4())
+		snapshot_volume = None
+		remove_directory = False
+		directory_mounted = False
+		current_cwd = os.getcwd()
+
+		try:
+			snapshot_volume = logical_volume.create_snapshot(snapshot_size, snapshot_suffix)
+			self.__logical_volume_uuid = logical_volume.uuid()
+			self.__snapshot = True
+
+			if mount_directory is None:
+				mount_directory = tempfile.mkdtemp(
+					suffix=snapshot_suffix, prefix=self.__class__.__mount_directory_prefix__
+				)
+				remove_directory = True
+			if mount_options is None:
+				mount_options = []
+			mount_options.insert(0, 'ro')
+
+			WMountPoint.mount(
+				logical_volume.volume_path(), mount_directory, fs=mount_fs, options=mount_options,
+				sudo=self.sudo()
+			)
+			directory_mounted = True
+
+			mount_directory_length = len(mount_directory)
+			for i in range(len(backup_sources)):
+				backup_sources[i] = backup_sources[i][mount_directory_length:]
+
+			os.chdir(mount_directory)
+			self._archive(abs_path=False)
+
+		except:
+			self.__logical_volume_uuid = None
+			self.__snapshot = False
+			raise
+		finally:
+			os.chdir(current_cwd)
+
+			if directory_mounted is True:
+				WMountPoint.umount(logical_volume.volume_path(), sudo=self.sudo())
+
+			if remove_directory is True:
+				os.removedirs(mount_directory)
+
+			if snapshot_volume is not None:
+				snapshot_volume.remove_volume()
+
+	def meta(self):
+		meta = WBackupTarArchiver.meta(self)
+		meta['snapshot'] = self.__snapshot
+		meta['lv_uuid'] = self.__logical_volume_uuid if self.__logical_volume_uuid is not None else ''
+		return meta
