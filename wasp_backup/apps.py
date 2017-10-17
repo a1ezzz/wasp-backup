0ce53b80ea45abdb47a5ff4fa04972034345c88e
Ildar Gafurov <dev@binblob.com>
2017-10-17 22:30:57 +0300

    wasp_backup/apps.py: broker commands updated

--- wasp_backup/apps.py
+++ wasp_backup/apps.py
@@ -27,17 +27,16 @@ from wasp_backup.version import __author__, __version__, __credits__, __license_
 # noinspection PyUnresolvedReferences
 from wasp_backup.version import __status__
 
-import traceback
-
 from wasp_general.verify import verify_type
 from wasp_general.command.command import WCommandResult
 from wasp_general.command.enhanced import WCommandArgumentDescriptor
 from wasp_general.crypto.aes import WAESMode
 from wasp_general.task.scheduler.task_source import WInstantTaskSource
 
-from wasp_launcher.core import WCommandKit, WAppsGlobals
+from wasp_launcher.core import WAppsGlobals
 from wasp_launcher.core_scheduler import WLauncherScheduleTask, WSchedulerTaskSourceInstaller, WLauncherTaskSource
-from wasp_launcher.apps.broker_commands import WBrokerCommand
+from wasp_launcher.core_scheduler import WLauncherScheduleRecord
+from wasp_launcher.core_broker import WResponsiveBrokerCommand, WBrokerCommand, WCommandKit
 
 from wasp_backup.archiver import WBackupTarArchiver, WLVMBackupTarArchiver
 from wasp_backup.cipher import WBackupCipher
@@ -53,7 +52,7 @@ class WBackupBrokerCommandKit(WCommandKit):
 
 	@classmethod
 	def commands(cls):
-		return WBackupCommands.Backup(),
+		return WBackupCommands.Create(), WBackupCommands.Check()
 
 
 class WBackupSchedulerInstaller(WSchedulerTaskSourceInstaller):
@@ -65,9 +64,8 @@ class WBackupSchedulerInstaller(WSchedulerTaskSourceInstaller):
 		__task_source_name__ = 'com.binblob.wasp-backup.scheduler.sources.instant_source'
 
 		def __init__(self, scheduler):
-			WInstantTaskSource.__init__(self, scheduler, on_drop_callback=self.on_drop)
+			WInstantTaskSource.__init__(self, scheduler)
 			WLauncherTaskSource.__init__(self)
-			self.__scheduler = scheduler
 
 		def name(self):
 			return self.__task_source_name__
@@ -75,13 +73,6 @@ class WBackupSchedulerInstaller(WSchedulerTaskSourceInstaller):
 		def description(self):
 			return 'Backup tasks from broker'
 
-		def add_task(self, task):
-			WInstantTaskSource.add_task(self, task)
-			self.__scheduler.update(task_source=self)
-
-		@classmethod
-		def on_drop(cls, task):
-			WAppsGlobals.log.error('Some task was dropped: ' + str(task))
 
 	__registry_tag__ = 'com.binblob.wasp-backup.scheduler.sources'
 
@@ -104,7 +95,8 @@ class WBackupCommands:
 		'com.binblob.wasp-backup.scheduler.sources'
 	]
 
-	class Backup(WBrokerCommand):
+
+	class Create(WResponsiveBrokerCommand):
 
 		class CompressionArgumentHelper(WCommandArgumentDescriptor.ArgumentCastingHelper):
 
@@ -125,6 +117,7 @@ class WBackupCommands:
 					return
 				else:
 					raise ValueError('Invalid compression value')
+
 
 		class SchedulerTask(WLauncherScheduleTask):
 
@@ -149,9 +142,11 @@ class WBackupCommands:
 			def name(self):
 				return self.__task_name__
 
-			def description(self):
+			def brief_description(self):
 				return self.__task_description_prefix__ + (', '.join(self.__archiver.backup_sources()))
 
+
+		__command__ = 'create'
 
 		__arguments__ = [
 			WCommandArgumentDescriptor(
@@ -164,12 +159,12 @@ class WBackupCommands:
 			WCommandArgumentDescriptor(
 				'sudo', flag_mode=True,
 				help_info='use "sudo" command for privilege promotion. "sudo" may be used for snapshot \
-creation, partition mounting and un-mounting'
+	creation, partition mounting and un-mounting'
 			),
 			WCommandArgumentDescriptor(
 				'force-snapshot', flag_mode=True, help_info='force to use snapshot for backup. \
-By default, backup will try to make snapshot for input files, if it is unable to do so - then backup copy files as is. \
-			With this flag, if snapshot can not be created - backup will stop'
+	By default, backup will try to make snapshot for input files, if it is unable to do so - then backup copy files as is. \
+				With this flag, if snapshot can not be created - backup will stop'
 			),
 			WCommandArgumentDescriptor(
 				'snapshot-volume-size', meta_var='fraction_size',
@@ -181,12 +176,12 @@ By default, backup will try to make snapshot for input files, if it is unable to
 			WCommandArgumentDescriptor(
 				'snapshot-mount-dir', meta_var='mount_path',
 				help_info='path where snapshot volume should be mount. It is random directory by \
-default'
+	default'
 			),
 			WCommandArgumentDescriptor(
 				'compression', meta_var='compression_type',
 				help_info='compression option. One of: "gzip", "bzip2" or "disabled". It is disabled \
-by default', casting_helper=CompressionArgumentHelper()
+	by default', casting_helper=CompressionArgumentHelper()
 			),
 			WCommandArgumentDescriptor(
 				'password', meta_var='encryption_password',
@@ -195,7 +190,7 @@ by default', casting_helper=CompressionArgumentHelper()
 			WCommandArgumentDescriptor(
 				'cipher_algorithm', meta_var='algorithm_name',
 				help_info='cipher that will be used for encrypt (backup won\'nt be encrypted if \
-password wasn\'t set). It is "AES-256-CBC" by default',
+	password wasn\'t set). It is "AES-256-CBC" by default',
 				casting_helper=WCommandArgumentDescriptor.StringArgumentCastingHelper(
 					validate_fn=cipher_name_validation
 				),
@@ -203,12 +198,10 @@ password wasn\'t set). It is "AES-256-CBC" by default',
 			)
 		]
 
-		__scheduler_task_timeout__ = 3
+		__task_source_name__ = WBackupSchedulerInstaller.InstantTaskSource.__task_source_name__
+		__scheduler_instance__ = WBackupSchedulerInstaller.__scheduler_instance__
 
-		def __init__(self):
-			WBrokerCommand.__init__(self, 'backup', *WBackupCommands.Backup.__arguments__)
-
-		def _exec(self, command_arguments):
+		def create_task(self, command_arguments, **command_env):
 			compress_mode = None
 			if 'compression' in command_arguments.keys():
 				compress_mode = command_arguments['compression']
@@ -219,11 +212,6 @@ password wasn\'t set). It is "AES-256-CBC" by default',
 					command_arguments['cipher_algorithm'], command_arguments['password']
 				)
 
-			archiver = WLVMBackupTarArchiver(
-				command_arguments['output'], *command_arguments['input'], compress_mode=compress_mode,
-				sudo=command_arguments['sudo'], cipher=cipher
-			)
-
 			snapshot_size = None
 			if 'snapshot-volume-size' in command_arguments.keys():
 				snapshot_size = command_arguments['snapshot-volume-size']
@@ -232,37 +220,51 @@ password wasn\'t set). It is "AES-256-CBC" by default',
 			if 'snapshot-mount-dir' in command_arguments.keys():
 				snapshot_mount_dir = command_arguments['snapshot-mount-dir']
 
-			task_source = WAppsGlobals.scheduler.task_source(
-				WBackupSchedulerInstaller.InstantTaskSource.__task_source_name__,
-				WBackupSchedulerInstaller.__scheduler_instance__
+			archiver = WLVMBackupTarArchiver(
+				command_arguments['output'], *command_arguments['input'], compress_mode=compress_mode,
+				sudo=command_arguments['sudo'], cipher=cipher
 			)
 
-			if task_source is None:
-				return WCommandResult(
-					output='Unable to find suitable scheduler. Command rejected', error=1
-				)
-
-			scheduler_task = WBackupCommands.Backup.SchedulerTask(
+			return WBackupCommands.Create.SchedulerTask(
 				archiver, command_arguments['force-snapshot'], snapshot_size, snapshot_mount_dir
 			)
-			task_source.add_task(scheduler_task)
-
-			if scheduler_task.start_event().wait(self.__scheduler_task_timeout__) is False:
-				return WCommandResult(
-					output='Scheduler is busy at the moment. Backup task is registered and waits '
-					'for the scheduler',
-					error=1
-				)
-
-			uid = None
-			for task in task_source.scheduler_service().running_records():
-				if task.record().task() == scheduler_task:
-					uid = task.task_uid()
-
-			if uid is not None:
-				return WCommandResult(output='Backup task is running. Task uid: %s' % str(uid))
-			else:
-				return WCommandResult(output='Backup seems to be finished. Really fast!')
 
 		def brief_description(self):
-			return 'backup data'
+			return 'create backup archive of files and directories'
+
+	class Check(WResponsiveBrokerCommand):
+
+		class SchedulerTask(WLauncherScheduleTask):
+
+			def __init__(self, archive):
+				WLauncherScheduleTask.__init__(self)
+				self.__archive = archive
+
+			def thread_started(self):
+				pass
+
+			def name(self):
+				return 'Archiving check task'
+
+			def brief_description(self):
+				return 'Checking file: ' + self.__archive
+
+			def state_details(self):
+				return None
+
+		__command__ = 'check'
+		__arguments__ = [
+			WCommandArgumentDescriptor(
+				'archive', required=True, multiple_values=False, meta_var='archive_path',
+				help_info='backup file to check'
+			)
+		]
+
+		__task_source_name__ = WBackupSchedulerInstaller.InstantTaskSource.__task_source_name__
+		__scheduler_instance__ = WBackupSchedulerInstaller.__scheduler_instance__
+
+		def create_task(self, command_arguments, **command_env):
+			return WBackupCommands.Check.SchedulerTask(command_arguments['archive'])
+
+		def brief_description(self):
+			return 'check backup archive for integrity'
