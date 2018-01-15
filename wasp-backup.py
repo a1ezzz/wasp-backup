3f404471503a547dc05ddac082470a400621ba83
Ildar Gafurov <dev@binblob.com>
2018-01-15 16:10:02 +0300

    wasp-backup.py: help command was added

--- wasp-backup.py
+++ wasp-backup.py
@@ -29,7 +29,8 @@ import sys
 import os
 from logging import getLogger
 
-from wasp_general.command.command import WCommandSet, WCommandProto
+from wasp_general.command.command import WCommandSet, WCommandProto, WCommandPrioritizedSelector, WCommand
+from wasp_general.command.result import WPlainCommandResult
 
 from wasp_backup.file_backup import WFileBackupCommand
 from wasp_backup.check import WCheckBackupCommand
@@ -37,14 +38,38 @@ from wasp_backup.program_backup import WProgramBackupCommand
 from wasp_backup.retention import WRetentionBackupCommand
 
 
+class WCommandHelp(WCommand):
+
+	__help_info__ = '''This utility is able to create file or program backup, to check archive integrity and, \
+and is able to rotate archives that resides locally or on a remote location.
+Syntax: %s <main_command> [<command argument 1> <command argument 2> <command argument 3>...]
+
+''' % sys.argv[0]
+
+	def __init__(self, command_selector):
+		WCommand.__init__(self, 'help')
+		self.__command_selector = command_selector
+
+	def _exec(self, *command_tokens, **command_env):
+		result = self.__help_info__
+		for command in self.__command_selector.commands():
+			if isinstance(command, WCommandHelp) is True:
+				continue
+			result += 'Command "%s" is able to: %s\n' % (command.__command__, command.__description__)
+			result += command.command_help()
+			result += '\n\n'
+
+		return WPlainCommandResult(result)
+
+
 if __name__ == '__main__':
 
 	logger = getLogger(os.path.basename(sys.argv[0]))
 
-	command_set = WCommandSet()
-	command_set.commands().add(WFileBackupCommand(logger))
-	command_set.commands().add(WCheckBackupCommand(logger))
-	command_set.commands().add(WProgramBackupCommand(logger))
-	command_set.commands().add(WRetentionBackupCommand(logger))
-
+	command_set = WCommandSet(command_selector=WCommandPrioritizedSelector())
+	command_set.commands().add_prioritized(WCommandHelp(command_set), 30)
+	command_set.commands().add_prioritized(WFileBackupCommand(logger), 50)
+	command_set.commands().add_prioritized(WProgramBackupCommand(logger), 50)
+	command_set.commands().add_prioritized(WCheckBackupCommand(logger), 50)
+	command_set.commands().add_prioritized(WRetentionBackupCommand(logger), 50)
 	print(command_set.exec(WCommandProto.join_tokens(*(sys.argv[1:]))))
